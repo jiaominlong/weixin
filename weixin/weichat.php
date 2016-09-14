82e2baeae844b5b56ba092a9048ab9c8c9bc1b05
jml <jiaominlong@outlook.com>
2016-09-14 15:47:17 +0800

    add weichat

---
+++ weixin/weichat.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jml
+ * Date: 2016/9/14
+ * Time: 15:08
+ */
+require_once '../config.php';
+
+class weichat{
+
+    public function callbackWeiChat(){
+        $returnStr = $_GET['echostr'];
+        if ($this->checkSignature()){
+            echo $returnStr;
+        }else{
+            echo "<b>Error:</b>".$returnStr;
+        }
+    }
+
+    private function checkSignature(){
+        $signature = $_GET['signature'];
+        $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+
+        $tmpArr = array(TOKEN,$timestamp,$nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if($tmpStr == $signature){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
